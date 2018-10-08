a034621cb565bb8a9cd4acd9b234dd42dfe97ec0
teixeirazeus <teixeira.zeus@gmail.com>
2018-10-08 17:58:25 -0300

    test

--- medusa.c
+++ medusa.c
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
 
   /* Imprime informacao sobre versao e como sair */
-  puts("Medusa Version 0.0.0.0.1");
+  puts("Medusa Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
 
   /* Num laco que nunca acaba */
